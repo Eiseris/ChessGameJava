e5a9e82c4c2d97ddc7ec2fc1950045d8c3c76745
Eiseris <115601804+Eiseris@users.noreply.github.com>
2024-04-01 09:05:26 -0500

    Cleaning

--- ChessGame/src/chessgame/ChessGame.java
+++ ChessGame/src/chessgame/ChessGame.java
@@ -1,4 +1,3 @@
-
 package chessgame;
 
 import java.awt.Color;
@@ -15,160 +14,142 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
 public class ChessGame {
-    public static LinkedList<Piece> ps=new LinkedList<>();
+    public static LinkedList<Piece> pieces = new LinkedList<>();
     public static Piece selectedPiece = null;
+
     public static void main(String[] args) throws IOException {
-        BufferedImage all=ImageIO.read(new File("C:\\chess.png"));
-        Image imgs[]=new Image[12];
-       int ind=0;
-        for(int y=0;y<400;y+=200){
-        for(int x=0;x<1200;x+=200){
-            imgs[ind]=all.getSubimage(x, y, 200, 200).getScaledInstance(64, 64, BufferedImage.SCALE_SMOOTH);
-       ind++;
-        }    
+        BufferedImage all = ImageIO.read(new File("C:\\chess.png"));
+        Image[] imgs = new Image[12];
+        int ind = 0;
+        for (int y = 0; y < 400; y += 200) {
+            for (int x = 0; x < 1200; x += 200) {
+                imgs[ind] = all.getSubimage(x, y, 200, 200).getScaledInstance(64, 64, BufferedImage.SCALE_SMOOTH);
+                ind++;
+            }
         }
-        
-                
+
         JFrame frame = new JFrame();
         frame.setBounds(10, 10, 512, 512);
         frame.setUndecorated(true);
-        JPanel pn=new JPanel(){
+        JPanel panel = createBoard(imgs);
+        frame.add(panel);
+        addMouseListeners(frame, panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        initializePieces();
+    }
+
+    public static JPanel createBoard(Image[] imgs) {
+        return new JPanel() {
             @Override
             public void paint(Graphics g) {
-            boolean white=true;
-                for(int y= 0;y<8;y++){
-            for(int x= 0;x<8;x++){
-                if(white){
-                    g.setColor(new Color(235,235, 208));
-                }else{
-                    g.setColor(new Color(119, 148, 85));
-                    
+                boolean white = true;
+                for (int y = 0; y < 8; y++) {
+                    for (int x = 0; x < 8; x++) {
+                        g.setColor(white ? new Color(235, 235, 208) : new Color(119, 148, 85));
+                        g.fillRect(x * 64, y * 64, 64, 64);
+                        white = !white;
+                    }
+                    white = !white;
                 }
-                g.fillRect(x*64, y*64, 64, 64);
-           white=!white;
-            }
-            white=!white;
-            }
-                for(Piece p: ps){
-                    int ind=0;
-                    if(p.name.equalsIgnoreCase("king")){
-                        ind=0;
-                    }
-                    if(p.name.equalsIgnoreCase("queen")){
-                        ind=1;
-                    }
-                    if(p.name.equalsIgnoreCase("bishop")){
-                        ind=2;
-                    }
-                    if(p.name.equalsIgnoreCase("knight")){
-                        ind=3;
-                    }
-                    if(p.name.equalsIgnoreCase("rook")){
-                        ind=4;
-                    }
-                    if(p.name.equalsIgnoreCase("pawn")){
-                        ind=5;
-                    }
-                    if(!p.isWhite){
-                        ind+=6;
+                for (Piece p : pieces) {
+                    int ind = p.isWhite ? 0 : 6;
+                    switch (p.name.toLowerCase()) {
+                        case "king":
+                            ind += 0;
+                            break;
+                        case "queen":
+                            ind += 1;
+                            break;
+                        case "bishop":
+                            ind += 2;
+                            break;
+                        case "knight":
+                            ind += 3;
+                            break;
+                        case "rook":
+                            ind += 4;
+                            break;
+                        case "pawn":
+                            ind += 5;
+                            break;
                     }
                     g.drawImage(imgs[ind], p.x, p.y, this);
                 }
             }
-            
         };
-        frame.add(pn);
-        frame.addMouseMotionListener(new MouseMotionListener() {
+    }
+
+    public static void addMouseListeners(JFrame frame, JPanel panel) {
+        panel.addMouseMotionListener(new MouseMotionListener() {
             @Override
-            public void mouseDragged(MouseEvent e){
-                if(selectedPiece != null) {
-                    selectedPiece.x = e.getX()-32;
-                    selectedPiece.y = e.getY()-32;
+            public void mouseDragged(MouseEvent e) {
+                if (selectedPiece != null) {
+                    selectedPiece.x = e.getX() - 32;
+                    selectedPiece.y = e.getY() - 32;
                     frame.repaint();
                 }
             }
 
             @Override
             public void mouseMoved(MouseEvent e) {
-
             }
         });
-        frame.addMouseListener(new MouseListener() {
-
+        panel.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                
             }
 
             @Override
             public void mousePressed(MouseEvent e) {
-                selectedPiece = getPiece(e.getX(), e.getY());
+                selectedPiece = getPieceAtLocation(e.getX(), e.getY());
             }
 
             @Override
             public void mouseReleased(MouseEvent e) {
-                selectedPiece.move(e.getX()/64, e.getY()/64);
-                frame.repaint();
+                if (selectedPiece != null) {
+                    selectedPiece.move(e.getX() / 64, e.getY() / 64);
+                    frame.repaint();
+                }
             }
 
             @Override
             public void mouseEntered(MouseEvent e) {
-                
             }
-            
+
             @Override
             public void mouseExited(MouseEvent e) {
-                
             }
-
-
-            
         });
-        frame.setDefaultCloseOperation(3);
-        frame.setVisible(true);
-
-        Piece blackrookPiece=new Piece(0, 0, false, "rook", ps);
-        Piece blackknightPiece=new Piece(1, 0, false, "knight", ps);
-        Piece blackbishopPiece=new Piece(2, 0, false, "bishop", ps);
-        Piece blackqueenPiece=new Piece(3, 0, false, "queen", ps);
-        Piece blackkingPiece=new Piece(4, 0, false, "king", ps);
-        Piece blackbishopPiece2=new Piece(5, 0, false, "bishop", ps);
-        Piece blackknightPIece2=new Piece(6, 0, false, "knight", ps);
-        Piece blackrookPiece2=new Piece(7, 0, false, "rook", ps);
-        Piece blackpawnPiece1=new Piece(1, 1, false, "pawn", ps);
-        Piece blackpawnPiece2=new Piece(2, 1, false, "pawn", ps);
-        Piece blackpawnPiece3=new Piece(3, 1, false, "pawn", ps);
-        Piece blackpawnPiece4=new Piece(4, 1, false, "pawn", ps);
-        Piece blackpawnPiece5=new Piece(5, 1, false, "pawn", ps);
-        Piece blackpawnPiece6=new Piece(6, 1, false, "pawn", ps);
-        Piece blackpawnPiece7=new Piece(7, 1, false, "pawn", ps);
-        Piece blackpawnPiece8=new Piece(0, 1, false, "pawn", ps);
-        
-        Piece whiterookPiece=new Piece(0, 7, true, "rook", ps);
-        Piece whiteknightPiece=new Piece(1, 7, true, "knight", ps);
-        Piece whitebishopPiece=new Piece(2, 7, true, "bishop", ps);
-        Piece whitequeenPiece=new Piece(3, 7, true, "queen", ps);
-        Piece whitekingPiece=new Piece(4, 7, true, "king", ps);
-        Piece whitebishopPiece2=new Piece(5, 7, true, "bishop", ps);
-        Piece whiteknightPiece2=new Piece(6, 7, true, "knight", ps);
-        Piece whiterookPiece2=new Piece(7, 7, true, "rook", ps);
-        Piece whitepawnPiece1=new Piece(1, 6, true, "pawn", ps);
-        Piece whitepawnPiece2=new Piece(2, 6, true, "pawn", ps);
-        Piece whitepawnPiece3=new Piece(3, 6, true, "pawn", ps);
-        Piece whitepawnPiece4=new Piece(4, 6, true, "pawn", ps);
-        Piece whitepawnPiece5=new Piece(5, 6, true, "pawn", ps);
-        Piece whitepawnPiece6=new Piece(6, 6, true, "pawn", ps);
-        Piece whitepawnPiece7=new Piece(7, 6, true, "pawn", ps);
-        Piece whitepawnPiece8=new Piece(0, 6, true, "pawn", ps);
     }
-    public static Piece getPiece(int x, int y) {
-        int xp = x/64;
-        int yp = y/64;
-        for (Piece p: ps){
-            if (p.xp == xp && p.yp == yp) {
-                return p;
+
+    public static void initializePieces() {
+        initializePiecesForColor(0, false);
+        initializePiecesForColor(7, true);
+    }
+
+    public static void initializePiecesForColor(int row, boolean isWhite) {
+        new Piece(0, row, isWhite, "rook", pieces);
+        new Piece(1, row, isWhite, "knight", pieces);
+        new Piece(2, row, isWhite, "bishop", pieces);
+        new Piece(3, row, isWhite, "queen", pieces);
+        new Piece(4, row, isWhite, "king", pieces);
+        new Piece(5, row, isWhite, "bishop", pieces);
+        new Piece(6, row, isWhite, "knight", pieces);
+        new Piece(7, row, isWhite, "rook", pieces);
+        for (int i = 0; i < 8; i++) {
+            new Piece(i, row + (isWhite ? -1 : 1), isWhite, "pawn", pieces);
+        }
+    }
+
+    public static Piece getPieceAtLocation(int x, int y) {
+        int row = x / 64;
+        int col = y / 64;
+        for (Piece piece : pieces) {
+            if (piece.xp == row && piece.yp == col) {
+                return piece;
             }
         }
         return null;
